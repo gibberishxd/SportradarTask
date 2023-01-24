c9219e4d49a68813931bf575382631c7b669ef71
gibberishxd <gogiberidzetornike8@gmail.com>
2023-01-24 16:38:59 +0100

    JSON structure and import optimization.

--- src/DataMap.java
+++ src/DataMap.java
@@ -1,20 +1,115 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
 
 public class DataMap {
-    private static ObjectMapper objectMapper = getDefaultObjectMapper();
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class EventList {
 
-    private static ObjectMapper getDefaultObjectMapper() {
-        ObjectMapper defaultObjectMapper = new ObjectMapper();
-        // ---
-        return defaultObjectMapper;
+        @JsonProperty("Events")
+        private List<Event> events;
+
+        public List<Event> getEvents() {
+            return events;
+        }
+
+        public void setEvents(List<Event> events) {
+            this.events = events;
+        }
     }
 
-    public static JsonNode parse(String src) throws JsonProcessingException {
-        return objectMapper.readTree(src);
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    class Event {
+
+        @JsonProperty("sport_event_id")
+        private String sportEventId;
+
+        @JsonProperty("start_date")
+        private String startDate;
+
+        @JsonProperty("sport_name")
+        private String sportName;
+
+        @JsonProperty("competition_name")
+        private String competitionName;
+
+        @JsonProperty("competition_id")
+        private String competitionId;
+
+        @JsonProperty("season_name")
+        private String seasonName;
+
+        @JsonProperty("competitors")
+        private List<Competitor> competitors;
+
+        @JsonProperty("venue")
+        private Venue venue;
+
+        @JsonProperty("probability_home_team_winner")
+        private double probabilityHomeTeamWinner;
+
+        @JsonProperty("probability_draw")
+        private double probabilityDraw;
+
+        @JsonProperty("probability_away_team_winner")
+        private double probabilityAwayTeamWinner;
+
+        // getters and setters
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    class Competitor {
+
+        @JsonProperty("id")
+        private String id;
+
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("country")
+        private String country;
+
+        @JsonProperty("country_code")
+        private String countryCode;
+
+        @JsonProperty("abbreviation")
+        private String abbreviation;
+
+        @JsonProperty("qualifier")
+        private String qualifier;
+
+        @JsonProperty("gender")
+        private String gender;
+
+        // getters and setters
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    class Venue {
+
+        @JsonProperty("id")
+        private String id;
+
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("capacity")
+        private int capacity;
+
+        @JsonProperty("city_name")
+        private String cityName;
+
+        @JsonProperty("country_name")
+        private String countryName;
+
+        @JsonProperty("map_coordinates")
+        private String mapCoordinates;
+
+        @JsonProperty("country_code")
+        private String countryCode;
+
+        // getters and setters
     }
 }
